5ea7001b6a2bbcee190b687ad285628bede24a31
wushunjun <315497331@qq.com>
2019-04-14 13:58:17 +0800

    新上传

--- keywordModule/modules/report.js
+++ keywordModule/modules/report.js
@@ -10,25 +10,28 @@
 
 layui.define(function (exports) {
     var $ = layui.$
-    layui.use('table', function(){
+    layui.use('table', function () {
         var table = layui.table;
 
         table.render({
             elem: '#rank_table'
-            ,url:'kw_report_data.php'
-            ,cols: [[
-                {field:'keyword', width:180, title: '关键词'}
-                ,{field:'engines', width:180, title: '搜索引擎', templet: function(res){
-                    return '<img style="height: 40px;" src="./keywordModule/image/'+res.engines+'.png">';
-                }}
-                ,{field:'client', width:180, title: '引擎来源'}
-                ,{field:'rank', title: '排名', minWidth: 250}
-                ,{field:'source', width:180, title: '主要来源'}
-                ,{field:'collect_count', width:180, title: '收录量'}
+            , url: 'kw_report_data.php'
+            , cols: [[
+                {field: 'keyword', width: 180, title: '关键词'}
+                , {
+                    field: 'engines', width: 180, title: '搜索引擎', templet: function (res) {
+                        return '<img style="height: 40px;" src="./keywordModule/image/' + res.engines + '.png">';
+                    }
+                }
+                , {field: 'client', width: 180, title: '引擎来源'}
+                , {field: 'rank', title: '排名', minWidth: 250}
+                , {field: 'source', width: 180, title: '主要来源'}
+                , {field: 'collect_count', width: 180, title: '收录量'}
             ]]
-            ,page: true
+            , page: true
         });
-        $("body").on('click','.layui-form-radio',function(){console.log($('form').serialize());
+        $("body").on('click', '.layui-form-radio', function () {
+            console.log($('form').serialize());
             var client = $("input[name='client']:checked").val();
             var engines = $("input[name='engines']:checked").val();
             //执行重载
@@ -36,11 +39,160 @@ layui.define(function (exports) {
                 page: {
                     curr: 1 //重新从第 1 页开始
                 }
-                ,where: {
+                , where: {
                     client: client,
                     engines: engines,
                 }
             });
         })
     });
+    //区块轮播切换
+    layui.use(['admin', 'carousel'], function () {
+        var $ = layui.$
+            , admin = layui.admin
+            , carousel = layui.carousel
+            , element = layui.element
+            , device = layui.device();
+
+        //轮播切换
+        $('.layadmin-carousel').each(function () {
+            var othis = $(this);
+            carousel.render({
+                elem: this
+                , width: '100%'
+                , arrow: 'none'
+                , interval: othis.data('interval')
+                , autoplay: othis.data('autoplay') === true
+                , trigger: (device.ios || device.android) ? 'click' : 'hover'
+                , anim: othis.data('anim')
+            });
+        });
+
+        element.render('progress');
+
+    });
+    //图表
+    layui.use(['carousel', 'echarts'], function () {
+        var $ = layui.$
+            , carousel = layui.carousel
+            , echarts = layui.echarts,
+            data_json = eval('(' + $("#data_json").val() + ')');
+            console.log(data_json)
+        var echartsApp = [], options = [
+            {
+                tooltip: {
+                    trigger: 'axis'
+                },
+                legend: {
+                    x: 'center',
+                    y: 'bottom',
+                    data: ['上线情况']
+                },
+                polar: [
+                    {
+                        indicator: [
+                            {text: '百度PC'+data_json['Baidu']['pc']+'个', max: data_json['max']},
+                            {text: '360PC'+data_json['Haosou']['pc']+'个', max: data_json['max']},
+                            {text: '搜狗PC'+data_json['Sogou']['pc']+'个', max: data_json['max']},
+                            {text: '百度移动'+data_json['Baidu']['mobile']+'个', max: data_json['max']},
+                            {text: '360移动'+data_json['Haosou']['mobile']+'个', max: data_json['max']},
+                            {text: '搜狗移动'+data_json['Sogou']['mobile']+'个', max: data_json['max']}
+                        ],
+                        radius: "66%"
+                    }
+                ],
+                series: [
+                    {
+                        type: 'radar',
+                        center: ['50%', '50%'],
+                        itemStyle: {
+                            normal: {
+                                areaStyle: {
+                                    type: 'default'
+                                }
+                            }
+                        },
+                        data: [
+                            {value: [data_json['Baidu']['pc'], data_json['Haosou']['pc'], data_json['Sogou']['pc'], data_json['Baidu']['mobile'], data_json['Haosou']['mobile'], data_json['Sogou']['mobile']], name: '上线情况'},
+                            //{value: [data_json['Baidu']['pc'], 0, data_json['Sogou']['pc'], 80, 30, 50], name: '上线情况'},
+                        ]
+                    }
+                ]
+            }
+        ]
+            , elemDataView = $('#LAY-index-pageone').children('div')
+            , renderDataView = function (index) {
+            echartsApp[index] = echarts.init(elemDataView[index], layui.echartsTheme);
+            echartsApp[index].setOption(options[index]);
+            window.onresize = echartsApp[index].resize;
+        };
+        //没找到DOM，终止执行
+        if (!elemDataView[0]) return;
+
+        renderDataView(0);
+    });
+    layui.use(['carousel', 'echarts'], function () {
+        var $ = layui.$
+            , carousel = layui.carousel
+            , echarts = layui.echarts,
+            data_json = eval('(' + $("#data_json").val() + ')');
+        var echartsApp = [], options = [
+            {
+                tooltip: {
+                    trigger: 'item',
+                },
+                legend: {
+                    orient: 'horizontal', // 'vertical'
+                    icon: 'pie',
+                    // orient: 'vertical',
+                    x: 'center',
+                    y: 'bottom',
+                    selectedMode: true,
+                    data: ['PC端'+data_json['pc'], '移动端'+data_json['mobile']]
+                },
+                series: [
+                    {
+                        name: '关键词上线情况',
+                        center: ['50%', '50%'],
+                        type: 'pie',
+                        radius: ['50%', '65%'],
+                        avoidLabelOverlap: false,
+                        label: {
+                            normal: {
+                                show: false,
+                                position: 'center',
+                            },
+                            emphasis: {
+                                show: true,
+                                textStyle: {
+                                    fontSize: '20',
+                                    fontWeight: 'bold'
+                                }
+                            }
+                        },
+                        labelLine: {
+                            normal: {
+                                show: false
+                            }
+                        },
+                        data: [
+                            {value: data_json['mobile'], name: '移动端'+data_json['mobile']},
+                            {value: data_json['pc'], name: 'PC端'+data_json['pc']},
+                        ]
+                    }
+                ]
+            }
+        ]
+            , elemDataView = $('#LAY-index-pageone1').children('div')
+            , renderDataView = function (index) {
+            echartsApp[index] = echarts.init(elemDataView[index], layui.echartsTheme);
+            echartsApp[index].setOption(options[index]);
+            window.onresize = echartsApp[index].resize;
+        };
+        //没找到DOM，终止执行
+        if (!elemDataView[0]) return;
+
+        renderDataView(0);
+    });
+    exports('report', {})
 });
--- kw_report.php
+++ kw_report.php
@@ -17,19 +17,21 @@ if ($row['status'] == 0) {
         $dsql->ExecuteNoneQuery("DELETE FROM `#@__keywords_rank`");
         foreach ($result['Result']['Data'] as $k => $v) {
             foreach ($v as $key => $val) {
-                $keyword = $val['Keyword'];
                 $engines = str_replace("Pc", "", str_replace("Mobile", "", $key));
-                $collect_count = $val['Result']['CollectCount'];
-                $client = stripos($key, 'Mobile') !== false ? '移动' : 'PC';
-                $rank_str = '';
-                foreach ($val['Result']['Ranks'] as $item => $value) {
-                    $rank = explode('-', $value['RankStr']);
-                    $rank_str .= '第' . $rank[0] . '页，第' . $rank[1] . '条；';
-                    $source = $value['Url'];
+                if(in_array($engines, ['Baidu','Sogou','Haosou'])){
+                    $keyword = $val['Keyword'];
+                    $collect_count = $val['Result']['CollectCount'];
+                    $client = stripos($key, 'Mobile') !== false ? '移动' : 'PC';
+                    $rank_str = '';
+                    foreach ($val['Result']['Ranks'] as $item => $value) {
+                        $rank = explode('-', $value['RankStr']);
+                        $rank_str .= '第' . $rank[0] . '页，第' . $rank[1] . '条；';
+                        $source = $value['Url'];
+                    }
+                    $time = time();
+                    $query = "INSERT INTO `#@__keywords_rank`(keyword,engines,collect_count,client,source,rank,time) Values('$keyword','$engines','$collect_count','$client','$source','$rank_str','$time')";
+                    $dsql->ExecuteNoneQuery($query);
                 }
-                $time = time();
-                $query = "INSERT INTO `#@__keywords_rank`(keyword,engines,collect_count,client,source,rank,time) Values('$keyword','$engines','$collect_count','$client','$source','$rank_str','$time')";
-                $dsql->ExecuteNoneQuery($query);
             }
         }
         $dsql->ExecuteNoneQuery("update `#@__keywords_task` set status = 1");
@@ -37,6 +39,40 @@ if ($row['status'] == 0) {
 }
 $row = $dsql->GetOne("Select task_id,status,add_time From `#@__keywords_task` WHERE status = 1 ORDER BY aid DESC limit 1 ; ");
 $row['add_time'] = date('Y-m-d H:i:s',$row['add_time']);
+$query_data = "select * from #@__keywords_rank $where order by engines desc";
+$dsql->Execute('me',$query_data);
+$data = [];
+$max = $totle = $before_three = 0;
+while($arr = $dsql->GetArray())
+{
+    switch ($arr['client']) {
+        case 'PC':
+            $index = 'pc';
+            break;
+        case '移动':
+            $index = 'mobile';
+            break;
+        
+        default:
+            break;
+    }
+    $rank_arr = explode('；', $arr['rank']);
+    foreach($rank_arr as $k=>$v){
+        if(stripos($v, '第1页') !== false || stripos($v, '第2页') !== false || stripos($v, '第3页') !== false){
+            $before_three ++;
+        }
+    }
+    $data[$arr['engines']][$index] += (count($rank_arr) - 1);
+    if($data[$arr['engines']][$index] > $max){
+        $max = $data[$arr['engines']][$index];
+    }
+    $data[$index] += $data[$arr['engines']][$index];
+    $totle += $data[$arr['engines']][$index];
+}
+$data['totle'] = $totle;
+$data['before_three'] = $before_three;
+$data['max'] = str_replace(substr($max,-1),0,$max) + 10;
+$data_json = json_encode($data);
 $dlist = new DataListCP();
 $dlist->SetTemplet(DEDEADMIN . "/templets/kw_report.htm");
 $dlist->display();
